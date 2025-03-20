d511c9a6a70a9d1159e93fff09bb2874e3974523
mighty-baseplate <jn.atharva@gmail.com>
2025-03-20 09:59:06 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ReportIssue from './pages/ReportIssue';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/report" element={<ReportIssue />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
